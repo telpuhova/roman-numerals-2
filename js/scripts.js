d22441ceac00acdddaecfcce9b5616805cc2caae
julia sheremet and Natalia Telpukhova <natalia@>
2017-12-07 10:05:27 -0800

    add spec: value that is equal to a single roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 //Back End Logic
 
 var numberToRomanNumeral = function(inputNumber) {
+  // inputNumber = parseInt(inputNumber);
+  var symbols = ["I", "V", "X", "L", "C", "D", "M"];
+  var keyNumbers = ["1", "5", "10", "50", "100", "500", "1000"];
+
   if (!(inputNumber.match(/[0-9]/))) {
     return "Error!";
+  }
+  else {
+    for (var i = 0; i < keyNumbers.length; i++) {
+      if (inputNumber === keyNumbers[i]) {
+        return symbols[i];
+      }
+    }
   }
   return "I";
 }
